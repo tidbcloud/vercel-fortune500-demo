32ac83444c953d464166871578032c3b39205ae3
张冲 <zhangchong@192.168.1.2>
2023-01-18 21:18:25 +0800

    logo enter icon

--- components/SearchInput.js
+++ components/SearchInput.js
@@ -46,6 +46,25 @@ export const SearchInput = ({ onSearch, showingResult }) => {
     }
   }, [showingResult]);
 
+  const enterIcon = (
+    <svg
+      className={styles.enterIcon}
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M20 4V5.4C20 8.76031 20 10.4405 19.346 11.7239C18.7708 12.8529 17.8529 13.7708 16.7239 14.346C15.4405 15 13.7603 15 10.4 15H4M4 15L9 10M4 15L9 20"
+        stroke="black"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+
   return (
     <motion.div
       className={clsx(styles.wrapper, showingResult && styles.withResult)}
@@ -72,6 +91,7 @@ export const SearchInput = ({ onSearch, showingResult }) => {
           result={data}
           error={error}
         />
+        {enterIcon}
       </div>
 
       {showSuggestion && <Suggestions showingResult={showingResult} />}
