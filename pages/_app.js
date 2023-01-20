d81f8ed13eb6b4042d08b8eff87fe96605acbd73
zoubingwu <zoubingwu@gmail.com>
2023-01-20 22:49:46 +0800

    feat: add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
+import { Analytics } from "@vercel/analytics/react";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
