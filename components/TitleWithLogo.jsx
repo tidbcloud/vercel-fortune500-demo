eb33487aede8c7ce642af2d2abe90db333926653
Bosn <bosn@outlook.com>
2023-02-27 09:36:29 +0800

    feat: ai-insight home page 80%

---
+++ components/TitleWithLogo.jsx
@@ -0,0 +1,33 @@
+import { createStyles } from "@mantine/core";
+import Image from "next/image";
+
+const useStyles = createStyles(() => ({
+  main: {
+    display: 'flex',
+    width: 424,
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 66,
+    fontWeight: 600,
+  },
+}))
+
+export default function TitleWidthLogo() {
+  const { classes } = useStyles()
+
+  return (
+    <div className={classes.main}>
+      <div className="test">
+        <Image
+          src="/ai.svg"
+          alt="TiDB Cloud Logo"
+          width={100}
+          height={100}
+          priority
+        />
+      </div>
+      <div className={classes.title}>AI Insight</div>
+    </div>
+  )
+};
