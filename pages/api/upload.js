66df1fc0e0973805a8b9743681bcc1be8e90f910
Bosn <bosn@outlook.com>
2023-02-28 16:22:07 +0800

    feat: add error handler of upload CSV

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -12,52 +12,57 @@ function isNumeric(str) {
 export default async function handler(req, res) {
   const { content, filename } = req.body;
 
-  if (
-    req.method === "POST" &&
-    filename &&
-    content &&
-    typeof content === "string"
-  ) {
-    const [columns, data] = await parse(content);
+  try {
 
-    if (columns.some((i) => isNumeric(i))) {
-      // not a valid header
-      return res
-        .status(400)
-        .json({ message: "It seems this file does not contain valid header" });
+    if (
+      req.method === "POST" &&
+      filename &&
+      content &&
+      typeof content === "string"
+    ) {
+      const [columns, data] = await parse(content);
+
+      if (columns.some((i) => isNumeric(i))) {
+        // not a valid header
+        return res
+          .status(400)
+          .json({ message: "It seems this file does not contain valid header" });
+      }
+
+      const columnsSql = columns
+        .map((i, index) => sqlstring.format("?? TEXT", [i || `c${index}`]))
+        .join(",");
+
+      const db = DATABASE_ENV.database;
+      const table = generateUniqueName();
+
+      const createTableStatement =
+        sqlstring.format("CREATE TABLE IF NOT EXISTS ??.?? ", [db, table]) +
+        `(${columnsSql})`;
+
+      const insertStatement = sqlstring.format("INSERT INTO ??.?? VALUES ?", [
+        db,
+        table,
+        data,
+      ]);
+
+      await prisma.$transaction([
+        prisma.$executeRawUnsafe(createTableStatement),
+        prisma.$executeRawUnsafe(insertStatement),
+        prisma.file.create({
+          data: {
+            filename,
+            table,
+            hash: createHash("md5").update(content).digest("hex"),
+          },
+        }),
+      ]);
+
+      return res.status(200).json({ message: "success", data: { id: table } });
     }
 
-    const columnsSql = columns
-      .map((i, index) => sqlstring.format("?? TEXT", [i || `c${index}`]))
-      .join(",");
-
-    const db = DATABASE_ENV.database;
-    const table = generateUniqueName();
-
-    const createTableStatement =
-      sqlstring.format("CREATE TABLE IF NOT EXISTS ??.?? ", [db, table]) +
-      `(${columnsSql})`;
-
-    const insertStatement = sqlstring.format("INSERT INTO ??.?? VALUES ?", [
-      db,
-      table,
-      data,
-    ]);
-
-    await prisma.$transaction([
-      prisma.$executeRawUnsafe(createTableStatement),
-      prisma.$executeRawUnsafe(insertStatement),
-      prisma.file.create({
-        data: {
-          filename,
-          table,
-          hash: createHash("md5").update(content).digest("hex"),
-        },
-      }),
-    ]);
-
-    return res.status(200).json({ message: "success", data: { id: table } });
-  }
-
-  res.status(400).json({ message: "invalid request" });
+    res.status(400).json({ message: "invalid request" });
+    } catch(ex) {
+      res.status(500).json({ message: `Parse error: ${ex.message}, please try another file.` });
+    }
 }
