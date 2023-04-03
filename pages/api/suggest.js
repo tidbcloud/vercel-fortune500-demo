b9e333557238525d4f1c9483d7987fd16800760d
zoubingwu <zoubingwu@gmail.com>
2023-04-03 11:27:49 +0800

    fix: switch from edge runtime to node runtime

--- pages/api/suggest.js
+++ pages/api/suggest.js
@@ -1,22 +1,12 @@
 import { chat2question } from "@/lib/api";
 
-export const config = {
-  runtime: "edge",
-};
-
-export default async function handler(req) {
-  const { searchParams } = new URL(req.url);
-  const id = searchParams.get("id");
+export default async function handler(req, res) {
+  const id = req.query.id;
 
   if (req.method === "GET" && id) {
     const response = await chat2question(id);
-    return new Response(await response.text(), {
-      status: 200,
-      headers: {
-        "content-type": "application/json",
-      },
-    });
+    return res.status(200).json(await response.json());
   }
 
-  return new Response(null, { status: 400 });
+  return res.status(400).json({ message: "invalid request" });
 }
